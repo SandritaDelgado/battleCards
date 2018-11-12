c80116c417d541e5a60ff14e81373428d04b14d3
SandraDelgadoMartinez <manolovalverdejo@hotmail.com>
2018-11-12 12:37:03 +0000

    cambio interfaz

--- cliente/src/controlWeb.js
+++ cliente/src/controlWeb.js
@@ -1,44 +1,39 @@
-
 function limpiar(){
-	//inicializa la pagina
+	//inicializa la página
 	$('#formInicio').remove();
 }
 
 function mostrarFormularioNombre(){
 	var cadena='<div id="formInicio">';
-	cadena=cadena+'<h3>Iniciar sesion</h3>'
+	cadena=cadena+'<h3>Iniciar sesión</h3>';
 	cadena=cadena+'<input id="nombre" type="text" class="form-control" name="nombre" placeholder="Nombre usuario">';
 	cadena=cadena+'<button type="button" id="inicioBtn" class="btn btn-primary btn-md">Iniciar Usuario</button>';
 	cadena=cadena+'</div>';
-
-//hace referencia al inicio del html
+	
 	$('#inicio').append(cadena);
 
-//manejador del evento on click del boton
 	$('#inicioBtn').on('click',function(){
         var nombre=$('#nombre').val();
-        if(nombre==""){
+        if (nombre==""){
         	nombre="Loli";
         }
         $('#formInicio').remove();
         rest.agregarUsuario(nombre);
-    });
+     });
 }
 
 function mostrarCrearPartida(){
 	var cadena='<div id="formCrearPartida">';
-	cadena=cadena+'<h3>Iniciar Partida</h3>'
+	cadena=cadena+'<h3>Crear partida</h3>';
 	cadena=cadena+'<input id="nombre" type="text" class="form-control" name="nombre" placeholder="Nombre partida">';
-	cadena=cadena+'<button type="button" id="inicioBtn" class="btn btn-primary btn-md">Crear Partida</button>';
+	cadena=cadena+'<button type="button" id="inicioBtn" class="btn btn-primary btn-md">Crear partida</button>';
 	cadena=cadena+'</div>';
-
-//hace referencia al inicio del html
+	
 	$('#inicio').append(cadena);
 
-//manejador del evento on click del boton
 	$('#inicioBtn').on('click',function(){
         var nombrePartida=$('#nombre').val();
-        if(nombrePartida==""){
+        if (nombrePartida==""){
         	nombrePartida="prueba";
         }
         $('#formCrearPartida').remove();
@@ -46,19 +41,28 @@ function mostrarCrearPartida(){
     });
 }
 
-function mostrarListaPartidas(data){
+function mostrarListaPartidas(datos){
 
-	var cadena='<div id="formlistaPartidas">';
-	cadena=cadena+'<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Lista Partidas<span class="caret"></span></button>';
-	cadena=cadena+'<ul class="dropdown-menu">';
-	for(var i=0;i<data.lenght;i++){
-		cadena=cadena+'<li><a href="#">'+data[i].nombre+'</a></li>';
-	}
-	cadena=cadena+'</ul>';
-	cadena=cadena+'</div>';
+	$('#mostrarListaPartidas').remove();
+	var cadena='<div id="mostrarListaPartidas"><h3>Elegir partida</h3>';
+	cadena=cadena+'<div class="dropdown">';
+  	cadena=cadena+'<button class="btn btn-primary dropdown-toggle" id="mostrarListaBtn" type="button" data-toggle="dropdown">Elegir partida ';
+	cadena=cadena+'<span class="caret"></span></button>';
+  	cadena=cadena+'<ul id="dropdown" class="dropdown-menu">';
+  	cadena=cadena+'<li><a href="#">-</a></li>';
+  	for(var i=0;i<datos.length;i++){
+  		cadena=cadena+'<li><a href="#">'+datos[i].nombre+'</a></li>';
+  	}
+  	cadena=cadena+'</ul>';
+	cadena=cadena+'</div></div>';
+
 	$('#listaPartidas').append(cadena);
+
 	$('#mostrarListaBtn').on('click',function(){
-		var nombrePartida=$('#formlistaPartidas').val();
-	})
-  
+        var nombrePartida=$('#dropdown').val();
+        if (nombrePartida!=""){
+        	$('#mostrarListaPartidas').remove();
+        	com.elegirPartida(nombrePartida);
+        }
+    });
 }
